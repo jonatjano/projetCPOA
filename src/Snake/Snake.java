82d453a9cf490585043497be02cf8e384ff37f03
jonatjano <jonathan.selle@laposte.net>
2017-12-07 11:13:43 +0100

    on a enfin réussi, début du projet

--- src/Snake/Snake.java
+++ src/Snake/Snake.java
@@ -4,18 +4,17 @@
 package Snake;
 
 /**
- * @author jonathan
- *
+ * @author jonatjano
+ * @version 2017-12-07
  */
 public class Snake
 {
 
 	/**
-	 * @param args
-	 */fgdtrdsqdazer
+	 * @param args arguments de la commande (non-utilisés)
+	 */
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
 	}
-
 }
