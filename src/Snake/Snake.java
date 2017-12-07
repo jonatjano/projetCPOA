a1a49d0aa68847d5c234eab8d99b32dee33783a6
jonatjano <jonathan.selle@laposte.net>
2017-12-07 10:50:49 +0100

    creation de la classe principale

---
+++ src/Snake/Snake.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package Snake;
+
+/**
+ * @author jonathan
+ *
+ */
+public class Snake
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
